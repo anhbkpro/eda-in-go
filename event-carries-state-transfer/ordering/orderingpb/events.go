5ba17330fe4650ba232d2fa6a0e137a4220df942
anhlai <laingocanh1990@gmail.com>
2025-12-14 10:32:57 +0700

    feat(event-carries-state-transfer): add initial module setup with protobuf code generation
    - Add Makefile with install-tools and generate targets
    - Configure buf for protobuf code generation with go and go-grpc plugins
    - Add ordering service proto definitions (api.proto, events.proto)
    - Generate Go protobuf and gRPC code for ordering service
    - Add internal registry for serialization/deserialization
    - Include docker-compose configuration
    
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ event-carries-state-transfer/ordering/orderingpb/events.go
@@ -0,0 +1,39 @@
+package orderingpb
+
+import (
+	"eda-in-golang/internal/registry"
+	"eda-in-golang/internal/registry/serdes"
+)
+
+const (
+	OrderAggregateChannel = "mallbots.ordering.events.Order"
+
+	OrderCreatedEvent   = "ordering.api.OrderCreated"
+	OrderReadiedEvent   = "ordering.api.OrderReadied"
+	OrderCompletedEvent = "ordering.api.OrderCompleted"
+	OrderCanceledEvent  = "ordering.api.OrderCanceled"
+)
+
+func Registrations(reg registry.Registry) error {
+	serde := serdes.NewProtoSerdes(reg)
+
+	// Order events
+	if err := serde.Register(&OrderCreated{}); err != nil {
+		return err
+	}
+	if err := serde.Register(&OrderReadied{}); err != nil {
+		return err
+	}
+	if err := serde.Register(&OrderCompleted{}); err != nil {
+		return err
+	}
+	if err := serde.Register(&OrderCanceled{}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (*OrderCreated) Key() string   { return OrderCreatedEvent }
+func (*OrderReadied) Key() string   { return OrderReadiedEvent }
+func (*OrderCanceled) Key() string  { return OrderCanceledEvent }
+func (*OrderCompleted) Key() string { return OrderCompletedEvent }
